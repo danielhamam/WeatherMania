3b678e8c77e782c17bf429562db8a849076039f6
danielhamam <danielhamam@hotmail.com>
2022-03-29 22:49:03 -0400

    Display Weather Forecast API Info To UI

--- weathermania/routes/api.py
+++ weathermania/routes/api.py
@@ -1,23 +1,27 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
 from config import weather_api_key
 from weathermania.helpers import get_city_info
-import requests
+import requests, logging
 
 # Set this up as a blueprint. Name of blueprint
 bp = Blueprint('api', 'api')
 weather_api_url = "http://api.openweathermap.org/data"
 
 # --------------------------------------------------------------------------------------------
-# API Name: test
+# API Name: get_weather_forecast
 # Purpose: Returns a success message indicating the API services are working.
 # --------------------------------------------------------------------------------------------
 @bp.route("/get_weather_forecast", methods=['GET']) # tells Flask what URL should trigger the function
 def get_weather_forecast():
     # Get City Info API
-    city_info = get_city_info(request.args.get('city_name'))
-    payload = {'lat': city_info.get("lat"), 'lon': city_info.get("lon"), 'appid': weather_api_key}    
-    response = requests.get(
+    city_name = request.args.get('city_name')
+    if city_name is None:
+        raise Exception("City name argment not provided. Aborting...")
+    else:
+        city_info = get_city_info(request.args.get('city_name'))
+        payload = {'lat': city_info.get("lat"), 'lon': city_info.get("lon"), 'appid': weather_api_key}    
+        response = requests.get(
                 "https://api.openweathermap.org/data/2.5/weather",
                 params=payload
     )
-    return response.json()
+    return render_template("app.html", weather_data=response.json())
